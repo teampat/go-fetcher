1a2175aa080a437dc0f1ffd187e526f5176b0a79
teampat <teampat@gmail.com>
2018-07-22 01:53:34 +0700

    First commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Repos struct {
+	Name string `json:"Name"`
+	URL  string `json:"url"`
+}
+
+func main() {
+
+	res, _ := http.Get("http://www.mocky.io/v2/5b52fd522f0000510d3bb683")
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	var repos []Repos
+	err := json.Unmarshal(body, &repos)
+	if err != nil {
+		fmt.Println("There was an error:", err)
+	}
+	for _, repo := range repos {
+		fmt.Printf("Repo name: %s \n", repo.Name)
+		fmt.Printf("URL: %s \n", repo.URL)
+	}
+}
